0ddd653e2e744acb582b0620fce2beb8012aec82
parkchangju12 <jusu0369@naver.com>
2021-05-06 23:44:38 +0900

    first_test_App

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import DonkeyKong from "react-native-donkey-kong";
+
+export default class App extends Component {
+  render() {
+    return <DonkeyKong />;
+  }
+}
+
